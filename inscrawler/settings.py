9bb0d2b0928153d3a4356591988d5079eb671780
Faqihuddin Al Farisi <49012741+falfisme@users.noreply.github.com>
2019-10-06 21:06:25 +0700

    Add files via upload

---
+++ inscrawler/settings.py
@@ -0,0 +1,29 @@
+defaults = {
+    "fetch_likes_plays": True,
+    "fetch_likers": False,
+    "fetch_comments": True,
+    "fetch_mentions": True,
+    "fetch_hashtags": True,
+    "fetch_details": True
+}
+
+
+def apply_defaults(cls):
+    for name, value in defaults.items():
+        setattr(cls, name, value)
+    return cls
+
+
+@apply_defaults
+class settings(object):
+    pass
+
+
+def override_settings(args):
+    for name in defaults.keys():
+        setattr(settings, name, getattr(args, name))
+
+
+def prepare_override_settings(parser):
+    for name in defaults.keys():
+        parser.add_argument("--" + name, action="store_true")
